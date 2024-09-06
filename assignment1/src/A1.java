f0eb20a503685308606f8413523a8052fa9a5483
aviors902 <mguisard@outlook.com>
2024-09-07 00:16:48 +1000

    Fixed all present bugs
    FCFS, SRT and FBV now working
    
    Output formatting and LTR Scheduling still to do

--- assignment1/src/A1.java
+++ assignment1/src/A1.java
@@ -28,13 +28,6 @@ public class A1 {
 
     }
 
-
-
-
-
-
-
-
     public static List<List<String>> importFile(String filePath){
         File file = new File(filePath);
         /* Initializing the list of lists outside of the try block allows the list to be used outside of the Try block. */
@@ -54,12 +47,35 @@ public class A1 {
         }
         return linesList;
     }
+
+
+    public static void printSchedulerMovements(List<List<List<String>>> SchedulerOutput){
+        int i = 0;
+        List<List<String>> TimeStamps = SchedulerOutput.get(0);
+        List<List<String>> TurnAroundTimes = SchedulerOutput.get(1);
+
+        for ( List<String> TimeStamp : TimeStamps){
+            System.out.println(TimeStamp.get(0) + TimeStamp.get(1));
+        }
+
+        System.out.println("\nProcess  Turnaround Time  Waiting Time");
+    
+        while(i < TurnAroundTimes.size()){
+            System.out.println(TurnAroundTimes.get(i).get(0) + "        "+ TurnAroundTimes.get(i).get(1) + "               " + TurnAroundTimes.get(i).get(2));
+            i++;
+        }
+        System.out.println();
+        System.out.println("-------------------------------------------");
+        System.out.println();
+
+    }
+    
 }
 
 class Process {
     private String PID;
     private int  ArrTime, SrvTime, Tickets, TimeRemaining, lowPrio;
-    private int ElapsedTime = 0;
+    private int TurnAroundTime = 0;
 
     public Process(String ID, int Time, int ServeTime, int num){
         PID = ID;
@@ -86,8 +102,8 @@ class Process {
         Tickets = num;
     }
 
-    public void increaseElapsedTime(int increment){
-        ElapsedTime += increment;
+    public void updateTurnAroundTime(int currentTime){
+        TurnAroundTime = currentTime - ArrTime;
     }
 
     public void lowerTimeRemaining(int decrement){
@@ -121,8 +137,8 @@ class Process {
         return Tickets;
     }
 
-    public int getElapsedTime(){
-        return ElapsedTime;
+    public int getTurnAroundTime(){
+        return TurnAroundTime;
     }
 
     public int getTimeRemaining(){
@@ -307,7 +323,7 @@ class Scheduler{
                     previousPID = ReadyQueue.get(0).getPID();
 
                     for(int a = 0; a < ReadyQueue.size(); a++){
-                        ReadyQueue.get(a).increaseElapsedTime(1);
+                        ReadyQueue.get(a).updateTurnAroundTime(currentTime);
                     }
 
                 }
@@ -317,11 +333,6 @@ class Scheduler{
                 entry.add("T" + currentTime + ": ");
                 entry.add(previousPID);
                 TimeStamps.add(entry);
-                
-                
-
-
-
             }
             currentTime++;
         }
@@ -347,7 +358,7 @@ class Scheduler{
             Output += "\nProcess  Turnaround Time  Waiting Time\n";
             
             for(int i = 0; i < completedProcesses.size(); i++){
-                Output += completedProcesses.get(i).getPID() + "       " + Integer.toString(completedProcesses.get(i).getElapsedTime()) + "                " + (completedProcesses.get(i).getElapsedTime() - completedProcesses.get(i).getSrvTime()) + "\n";
+                Output += completedProcesses.get(i).getPID() + "       " + Integer.toString(completedProcesses.get(i).getTurnAroundTime()) + "                " + (completedProcesses.get(i).getTurnAroundTime() - completedProcesses.get(i).getSrvTime()) + "\n";
             }
         
         }
@@ -356,39 +367,36 @@ class Scheduler{
     }
 
     public static String FBV(List<Process> Processes, int Dispatcher){
+        String Output = "";
         List<List<String>> TimeStamps = new ArrayList<>();
-        List<Process> SortedProcesses = new ArrayList<>(Processes);
+        List<List<String>> TurnAroundTimes = new ArrayList<>();
+        List<Process> CopyOfProcesses = new ArrayList<>(Processes);
+        List<Process> SortedProcesses = sortListByArrTime(CopyOfProcesses);
         List<Process> HighPriority = new ArrayList<>();
         List<Process> MediumPriority = new ArrayList<>();
         List<Process> LowPriority = new ArrayList<>();
         List<Process> CompletedProcesses = new ArrayList<>();
         int i;
         int currentTime = 0;
-        String Output = "";
 
-        while(CompletedProcesses.size() != Processes.size()){
-
-
-
+        while(CompletedProcesses.size() != Processes.size()){         
             // Checking if new processes arrived
             for (i = 0; i < SortedProcesses.size(); i++) {
-                if (SortedProcesses.get(i).getArrTime() < currentTime) {
+                if (SortedProcesses.get(i).getArrTime() <= currentTime) {
                     HighPriority.add(SortedProcesses.get(i));
                     SortedProcesses.remove(i);
                     i--;
                 }
             }
 
+            // idle condition, no processes have arrived and all queues are empty
             if((HighPriority.isEmpty()) && (MediumPriority.isEmpty()) && (LowPriority.isEmpty())){
                 currentTime++;
-
-                List<String> entry = new ArrayList<>();
-                entry.add("T" + currentTime + ": ");
-                entry.add("Idle");
-                TimeStamps.add(entry);
             }
-
+            
             while(!HighPriority.isEmpty()){
+                currentTime += Dispatcher;
+
                 
                 // Output Information Capturing
                 List<String> entry = new ArrayList<>();
@@ -396,44 +404,61 @@ class Scheduler{
                 entry.add(HighPriority.get(0).getPID());
                 TimeStamps.add(entry);
 
+                // Time slice has been completed. If the remaining time is less than the full slice, the full slice is not consumed, only the time remaining
+                if(HighPriority.get(0).getTimeRemaining() >= 2){
+                    currentTime += 2;
+                }
+                else currentTime += HighPriority.get(0).getTimeRemaining();
+                
+
+                // Time Slice removed from remaining time for the process
                 HighPriority.get(0).lowerTimeRemaining(2);
+
+                // Recording the Turnaround Time for the processes in queue
                 for (i = 0; i < HighPriority.size(); i++){
-                    HighPriority.get(i).increaseElapsedTime(2);
+                    HighPriority.get(i).updateTurnAroundTime(currentTime);
+                }
+                for (i = 0; i < MediumPriority.size(); i++){
+                    MediumPriority.get(i).updateTurnAroundTime(currentTime);
+                }
+                for (i = 0; i < LowPriority.size(); i++){
+                    LowPriority.get(i).updateTurnAroundTime(currentTime);
                 }
 
+                // Checking if the current process is now completed. If not, lowering Priority
                 if(HighPriority.get(0).getTimeRemaining() == 0){
                     CompletedProcesses.add(HighPriority.get(0));
                 }
+                // Task is moved either to lower priority or to completed tasks, it will never remain in high prio
                 else MediumPriority.add(HighPriority.get(0));
-
                 HighPriority.remove(0);
-                // 2 ms slice has been consumed
-                currentTime += 2;
 
                 // Checking if new processes arrived during the time slice the high priority task was being executed
                 for (i = 0; i < SortedProcesses.size(); i++) {
-                    if (SortedProcesses.get(i).getArrTime() < currentTime) {
+                    if (SortedProcesses.get(i).getArrTime() <= currentTime) {
                         HighPriority.add(SortedProcesses.get(i));
                         SortedProcesses.remove(i);
                         i--;
                     }
                 }
-
+                // Checking the Time Elapsed for any processes in Low Prio. If it exceeds 16ms, they are moved to high prio.
                 if(!LowPriority.isEmpty()){
                     for(i = 0; i < LowPriority.size(); i++){
                         LowPriority.get(i).increaseLowPrioTicks(2);
                         if(LowPriority.get(i).getLowPrioTicks() > 16){
                             HighPriority.add(LowPriority.get(i));
                             LowPriority.remove(i);
+                            i--;
                         }
                     }
                 }
-
+                
+                
 
             }
-
-
+          
             while(!MediumPriority.isEmpty() && HighPriority.isEmpty()){
+                currentTime += Dispatcher;
 
                 // Output Information Capturing
                 List<String> entry = new ArrayList<>();
@@ -441,44 +466,57 @@ class Scheduler{
                 entry.add(MediumPriority.get(0).getPID());
                 TimeStamps.add(entry);
 
+                // Time Slice is consumed. If time remaining is less than the full slice, the full slice is not used, only the time remaining
+                if(MediumPriority.get(0).getTimeRemaining() >= 4){
+                    currentTime += 4;
+                }
+                else currentTime += MediumPriority.get(0).getTimeRemaining();
+
+                // Time slice for the process being executed
                 MediumPriority.get(0).lowerTimeRemaining(4);
+
+                // Tracking Turnaround time for all processes
                 for (i = 0; i < MediumPriority.size(); i++){
-                    MediumPriority.get(i).increaseElapsedTime(4);
+                    MediumPriority.get(i).updateTurnAroundTime(currentTime);
+                }
+                for (i = 0; i < LowPriority.size(); i++){
+                    LowPriority.get(i).updateTurnAroundTime(currentTime);
                 }
                 
-
+                // Checking if the process is completed, if not then lowering priority
                 if(MediumPriority.get(0).getTimeRemaining() == 0){
                     CompletedProcesses.add(MediumPriority.get(0));
                 }
                 else LowPriority.add(MediumPriority.get(0));
-
+                // Task is moved either to lower priority or to completed tasks, it will never remain in Medium prio
                 MediumPriority.remove(0);
-                // 4 ms slice has been consumed
-                currentTime += 4;
 
                 // Checking if new processes arrived during the time slice the medium priority task was being executed
                 for (i = 0; i < SortedProcesses.size(); i++) {
-                    if (SortedProcesses.get(i).getArrTime() < currentTime) {
+                    if (SortedProcesses.get(i).getArrTime() <= currentTime) {
                         HighPriority.add(SortedProcesses.get(i));
                         SortedProcesses.remove(i);
                         i--;
                     }
                 }
 
+                // Checking the Time Elapsed for any processes in Low Prio. If it exceeds 16ms, they are moved to high prio.
                 if(!LowPriority.isEmpty()){
                     for(i = 0; i < LowPriority.size(); i++){
                         LowPriority.get(i).increaseLowPrioTicks(4);
                         if(LowPriority.get(i).getLowPrioTicks() > 16){
                             HighPriority.add(LowPriority.get(i));
                             LowPriority.remove(i);
+                            i--;
                         }
                     }
-                }
-
+                } 
                 
             }
 
             while(!LowPriority.isEmpty() && HighPriority.isEmpty() && MediumPriority.isEmpty()){
+
+                currentTime += Dispatcher;
 
                 // Output Information Capturing
                 List<String> entry = new ArrayList<>();
@@ -486,65 +524,64 @@ class Scheduler{
                 entry.add(LowPriority.get(0).getPID());
                 TimeStamps.add(entry);
 
+                // Time Slice is consumed. If time remaining is less than the full slice, the full slice is not used, only the time remaining
+                if(LowPriority.get(0).getTimeRemaining() >= 4){
+                    currentTime += 4;
+                }
+                else currentTime += LowPriority.get(0).getTimeRemaining();
+
+                // Tracking the turnaround time for all tasks in the queue
                 LowPriority.get(0).lowerTimeRemaining(4);
                 for (i = 0; i < LowPriority.size(); i++){
-                    LowPriority.get(i).increaseElapsedTime(4);
+                    LowPriority.get(i).updateTurnAroundTime(currentTime);
                 }
 
+                // If process is complete, it is moved to the completed processes list, otherwise it remains in low priority queue
                 if(LowPriority.get(0).getTimeRemaining() == 0){
                     CompletedProcesses.add(LowPriority.get(0));
                     LowPriority.remove(0);
                 }
 
-                // 4 ms slice has been consumed
-                currentTime += 4;
-
                 // Checking if new processes arrived during the time slice the low priority task was being executed
                 for (i = 0; i < SortedProcesses.size(); i++) {
-                    if (SortedProcesses.get(i).getArrTime() < currentTime) {
+                    if (SortedProcesses.get(i).getArrTime() <= currentTime) {
                         HighPriority.add(SortedProcesses.get(i));
                         SortedProcesses.remove(i);
                         i--;
                     }
                 }
 
+                // Checking the Time Elapsed for any processes in Low Prio. If it exceeds 16ms, they are moved to high prio.
                 if(!LowPriority.isEmpty()){
                     for(i = 0; i < LowPriority.size(); i++){
                         LowPriority.get(i).increaseLowPrioTicks(4);
                         if(LowPriority.get(i).getLowPrioTicks() > 16){
+                            LowPriority.get(i).resetLowPrioTicks();
                             HighPriority.add(LowPriority.get(i));
                             LowPriority.remove(i);
+                            i--;
                         }
                     }
                 }
-            }
+                
+            } 
 
+        }
+        System.out.println("FBV");
+        for( List<String> timestamp : TimeStamps){
+            System.out.println(timestamp.get(0) + " " + timestamp.get(1));
+        }
+        System.out.println("\nProcess  Turnaround Time  Waiting Time");
+        i = 0;
+        while (i < CompletedProcesses.size()){
+            System.out.println(CompletedProcesses.get(i).getPID() + "        " + CompletedProcesses.get(i).getTurnAroundTime() + "               " + (CompletedProcesses.get(i).getTurnAroundTime()-CompletedProcesses.get(i).getSrvTime()));
+            i++;
+        }
             
-            
-
-        }
-
-        if(!TimeStamps.isEmpty()){
-            for(int x = 0; x < TimeStamps.size(); x++){
-                if(x < TimeStamps.size()-1){
-                    if((TimeStamps.get(x).get(1).equals(TimeStamps.get((x+1)).get(1))) || (TimeStamps.get(x+1).get(1).equals("Idle"))){
-                        TimeStamps.remove(x+1);
-                        x--;
-                    }
-
-                }
-            }
-            for (int t = 0; t < TimeStamps.size(); t++){
-                Output += (TimeStamps.get(t).get(0) + TimeStamps.get(t).get(1) + "\n");
-            }
-        }
-
-        for (i = 0; i < CompletedProcesses.size(); i++){
-            System.out.println(CompletedProcesses.get(i).getPID());
-        }
+        
+        
 
         return Output;
-    }
-    
-    
+        
+    } 
 }    
